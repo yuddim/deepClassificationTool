8d63637a0d3b5cd808415d82ac08ce593bbf07a8
Dmitry Yudin <yuddim@yandex.ru>
2018-03-17 12:59:51 +0300

    Add files via upload

--- deepClassificationTool.py
+++ deepClassificationTool.py
@@ -1,4 +1,4 @@
-
+import keras
 from keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
 from keras import optimizers
@@ -9,29 +9,29 @@ from modelVGGm import VGGm
 from testFunctions import test_image, test_two_image, test_image_gen, test_path_gen
 
 # dimensions of our images.
-img_width, img_height = 50, 50
+img_width, img_height = 135, 76
 
-train_data_dir = 'D:/Datasets/cars_angles/train'
-validation_data_dir = 'D:/Datasets/cars_angles/test'
+train_data_dir = 'D:/Datasets/copters/train'
+validation_data_dir = 'D:/Datasets/copters/test'
 
-train_tune_data_dir = 'D:/Datasets/cars_angles/train'
-validation_tune_data_dir = 'D:/Datasets/cars_angles/test'
+train_tune_data_dir = 'D:/Datasets/copters/train'
+validation_tune_data_dir = 'D:/Datasets/copters/test'
 
 epochs = 30
 tune_epochs = 10
 batch_size = 16
-train_flag = False
+train_flag = True
 tune_flag = False
 #weights_path = 'car_5angles_weights_50_30_inceptionv3_wtd.h5'#'cars_direct_1.h5'
-weights_path = 'car_5angles_weights_50_30_VGG.h5'
-weights_tuned_path = 'car_5angles_weights_50_1_inceptionv3_wtd.h5'#'cars_direct_tuned.h5'
+weights_path = 'camera_on_off_VGG.h5'
+weights_tuned_path = 'camera_on_off_tuned.h5'#'cars_direct_tuned.h5'
 
 if K.image_data_format() == 'channels_first':
     input_shape = (3, img_width, img_height)
 else:
     input_shape = (img_width, img_height, 3)
 
-#model = inceptionV3m(input_shape, 9)
+#model = inceptionV3m(input_shape, 5)
 #
 #model = inceptionV3Only(input_shape, 5)
 model = VGGm(input_shape, 5)
@@ -39,7 +39,7 @@ model = VGGm(input_shape, 5)
 
 
 #adam = optimizers.Adam(lr=1e-5)
-sgd = optimizers.SGD(lr=0.001, decay=0.0, momentum=0.9, nesterov=True)
+sgd = optimizers.SGD(lr=0.0001, decay=0.0, momentum=0.9, nesterov=True)
 
 if(train_flag == True):
     if(tune_flag):
@@ -97,10 +97,16 @@ if(train_flag == True):
     nb_train_samples = train_generator.samples
     nb_validation_samples = validation_generator.samples
 
+    callbacks = [keras.callbacks.ModelCheckpoint(
+        'D:/Projects/deepClassificationTool-master/models/camera_on_off_weights.{epoch:02d}-{val_loss:.2f}-{loss:.2f}.hdf5',
+        verbose=1,
+        save_weights_only=True)]
+
     hist = model.fit_generator(
         train_generator,
         steps_per_epoch=nb_train_samples // batch_size,
         epochs=epochs_train,
+        callbacks=callbacks,
         validation_data=validation_generator,
         validation_steps=nb_validation_samples // batch_size)
     stop = time.time()
@@ -125,13 +131,13 @@ else:
     print('Model is compiled\n')
 
 
-    test_path_gen('D:/Datasets/cars_angles/train', img_width, img_height, model,
-                  save_e_path='D:/Datasets/cars_angles/errorresults_special_train_resnet',
-                  save_tune_path='D:/Datasets/cars_angles/tune_special_train_resnet', save_errors=False,
+    test_path_gen('D:/Datasets/copters/train', img_width, img_height, model,
+                  save_e_path='D:/Datasets/copters/errorresults_special_train_vgg',
+                  save_tune_path='D:/Datasets/copters/tune_special_train_vgg', save_errors=True,
                   clear_tune_path=False)
-    test_path_gen('D:/Datasets/cars_angles/test', img_width, img_height, model,
-                  save_e_path='D:/Datasets/cars_angles/errorresults_special_test_resnet',
-                  save_tune_path='D:/Datasets/cars_angles/tune_special_test_resnet', save_errors=False, clear_tune_path=False)
+    test_path_gen('D:/Datasets/copters/test', img_width, img_height, model,
+                  save_e_path='D:/Datasets/copters/errorresults_special_vgg',
+                  save_tune_path='D:/Datasets/copters/tune_special_vgg', save_errors=True, clear_tune_path=False)
     """
     test_path_gen('D:/Datasets/cars_angles/validation', img_width, img_height, model,
                   save_e_path='D:/Datasets/cars_angles/errorresults_special_validation_resnet',
